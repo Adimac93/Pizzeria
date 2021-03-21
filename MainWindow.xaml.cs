7e73f44ce835906a77a29c2cba2ff8716b23b203
Adimac93 <Adimac93@gmail.com>
2021-03-21 21:35:47 +0100

    enabled statements

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,111 @@ namespace Pizzeria
         public MainWindow()
         {
             InitializeComponent();
+            gb1.IsEnabled = false;
+            gb2.IsEnabled = false;
+
+            bt3.IsEnabled = false;
+
+        }
+
+        private void bt1_Click(object sender, RoutedEventArgs e)
+        {
+            tb1a.Clear();
+        }
+
+        private void bt2_Click(object sender, RoutedEventArgs e)
+        {
+            cb1.SelectedIndex = -1;
+            
+        }
+
+        private void tb1a_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(tb1a.Text != "" && cb1.SelectedIndex != -1)
+            {
+                gb1.IsEnabled = true;
+                gb2.IsEnabled = true;
+
+                bt3.IsEnabled = true;
+            }
+            else
+            {                
+                gb1.IsEnabled = false;
+                gb2.IsEnabled = false;
+                bt3.IsEnabled = false;
+                /*
+                chb1.IsChecked = false;
+                chb2.IsChecked = false;
+                chb3.IsChecked = false;
+                chb4.IsChecked = false;
+
+                rb1.IsChecked = false;
+                rb2.IsChecked = false;
+                rb3.IsChecked = false;
+                rb4.IsChecked = false;
+                
+
+
+                tb1.Clear();
+                tb2.Clear();
+                tb3.Clear();
+                tb4.Clear();
+                */
+            }
+        }
+
+        private void cb1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(tb1a.Text != "" && cb1.SelectedIndex != -1)
+            {
+                gb1.IsEnabled = true;
+                gb2.IsEnabled = true;
+
+                bt3.IsEnabled = true;
+            }
+            else
+            {
+                gb1.IsEnabled = false;
+                gb2.IsEnabled = false;
+                bt3.IsEnabled = false;
+                /*
+                chb1.IsChecked = false;
+                chb2.IsChecked = false;
+                chb3.IsChecked = false;
+                chb4.IsChecked = false;
+
+                rb1.IsChecked = false;
+                rb2.IsChecked = false;
+                rb3.IsChecked = false;
+                rb4.IsChecked = false;
+                
+
+
+                tb1.Clear();
+                tb2.Clear();
+                tb3.Clear();
+                tb4.Clear();
+                */
+            }
+        }
+
+        private void btCancel_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Anulowałeś zamówienie","Uwaga");
+            this.Close();
+            
+
+        }
+
+        private void btAccept_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Złożono zamówienie", "Uwaga");
+            this.Close();
+        }
+
+        private void bt3_Click(object sender, RoutedEventArgs e)
+        {
+            lb1.Items.Insert(0, "ge");
         }
     }
 }
